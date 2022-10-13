ab42e5c7d2c9019f10f90ec815ca1fb6a256e4ca
缘兮， <51600219+yg-YuanXi@users.noreply.github.com>
2022-10-13 17:39:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oXHtN5v7hfaoROBsCvPWXpoepSNc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'hmrlXgEqqwM-WDsQbN6aQlcncxKc6vUJhndG-tw0zh0',
+      useTemplateId: 'V-5fYCzRkPQ6LfxwK61cbIgHdsiv9DzXariVgLqBSvQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-10',
       festivals: [
